c0b9c07ca5421a6c03eb2aea0c39204d00bcee0b
Bogdan Arabadzhi <bogdan.today@gmail.com>
2017-02-22 13:20:02 +0300

    add minimal number system solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function getBase(base) {
-  // your solution
+module.exports = function getBase(n) {
+    if (n.length > 13) {
+        return '9'.repeat(n.length - 1);
+    }
+
+    for (var base = 2, minimalBase = ''; base <= n; base++) {
+        for (var tmpN = +n; tmpN; tmpN = Math.floor(tmpN / base)) {
+            minimalBase = (tmpN % base === 1)
+                ? minimalBase + '1'
+                : '';
+
+            if (!minimalBase.length) {
+                break;
+            }
+        }
+
+        if (minimalBase.length) {
+            return base.toString();
+        }
+    }
 }
